7766d51a48d24fd03d14eada37f7015742446105
Popov Bogdan <Popov.B.V@sberbank.ru>
2018-06-27 23:20:47 +0400

    добавил пример с ключевым словом companion

---
+++ src/ru/popov/bodya/keywords/companion.kt
@@ -0,0 +1,35 @@
+package ru.popov.bodya.keywords
+
+class A {
+    companion object {
+        fun bar() {
+            println("Companion object called")
+        }
+    }
+}
+
+class User private constructor(val nickname: String) {
+    companion object {
+        fun newSubscribingUser(email: String) = User(email.substringBefore('@'))
+        fun newFacebookUser(accountId: Int) = User("Bodya$accountId")
+    }
+}
+
+interface JSONFactory<T> {
+    fun fromJSON(jsonText: String): T
+}
+
+class JsonPerson(val name: String) {
+    companion object : JSONFactory<JsonPerson> {
+        override fun fromJSON(jsonText: String): JsonPerson = JsonPerson(jsonText)
+    }
+}
+
+fun <T> loadFromJSON(factory: JSONFactory<T>) = factory.fromJSON("Hello")
+
+fun main(args: Array<String>) {
+    A.bar()
+    println(User.newSubscribingUser("popovbodya@google.com").nickname)
+    println(User.newFacebookUser(9).nickname)
+    println(loadFromJSON(JsonPerson).name)
+}
