d829b7b3ce01c8ad4a99b492984251a4bf00beb5
Popov Bogdan <popovbodya@gmail.com>
2018-07-05 15:49:02 +0300

    добавил арифметические операции расширения

--- src/ru/popov/bodya/overloading/operator.kt
+++ src/ru/popov/bodya/overloading/operator.kt
@@ -4,8 +4,16 @@ data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
 }
 
+private operator fun Point.times(other: Point) = Point(x * other.x, y * other.y)
+private operator fun Point.div(other: Point) = Point(x / other.x, y / other.y)
+private operator fun Point.rem(other: Point) = Point(x % other.x, y % other.y)
+private operator fun Point.minus(other: Point) = Point(x - other.x, y - other.y)
+
 fun main(args: Array<String>) {
     val p1 = Point(10, 20)
     val p2 = Point(30, 40)
     println(p1 + p2)
+    println(p1 / p2)
+    println(p1 % p2)
+    println(p1 - p2)
 }
