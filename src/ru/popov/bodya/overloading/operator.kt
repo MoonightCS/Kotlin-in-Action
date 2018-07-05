b97ca453c400debf1d671f3c0ae04a169a7ac535
Popov Bogdan <popovbodya@gmail.com>
2018-07-05 17:03:10 +0300

    добавил парочку операторов

--- src/ru/popov/bodya/overloading/operator.kt
+++ src/ru/popov/bodya/overloading/operator.kt
@@ -8,6 +8,15 @@ private operator fun Point.times(other: Point) = Point(x * other.x, y * other.y)
 private operator fun Point.div(other: Point) = Point(x / other.x, y / other.y)
 private operator fun Point.rem(other: Point) = Point(x % other.x, y % other.y)
 private operator fun Point.minus(other: Point) = Point(x - other.x, y - other.y)
+private operator fun Point.times(scale: Double) = Point((x * scale).toInt(), (y * scale).toInt())
+private operator fun <T> MutableCollection<T>.plusAssign(element: T) {
+    this.add(element)
+}
+private operator fun <T> MutableCollection<T>.plus(element: T) : MutableCollection<T> {
+    this.add(element)
+    return this;
+}
+private operator fun Point.unaryMinus() = Point(-x, -y)
 
 fun main(args: Array<String>) {
     val p1 = Point(10, 20)
@@ -16,4 +25,10 @@ fun main(args: Array<String>) {
     println(p1 / p2)
     println(p1 % p2)
     println(p1 - p2)
+    println(p1 * 3.0)
+    println(-p1)
+
+    val numbers = ArrayList<Int>()
+    numbers += 42
+    println(numbers)
 }
