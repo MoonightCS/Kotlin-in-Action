1aa88c95456770de3853bb4cee7b2056cbd77639
Popov Bogdan <popovbodya@gmail.com>
2018-07-06 14:22:23 +0300

    добавил примеры с операторами set и get

--- src/ru/popov/bodya/overloading/operator.kt
+++ src/ru/popov/bodya/overloading/operator.kt
@@ -12,11 +12,24 @@ private operator fun Point.times(scale: Double) = Point((x * scale).toInt(), (y 
 private operator fun <T> MutableCollection<T>.plusAssign(element: T) {
     this.add(element)
 }
-private operator fun <T> MutableCollection<T>.plus(element: T) : MutableCollection<T> {
+private operator fun Point.unaryMinus() = Point(-x, -y)
+private operator fun Point.get(index: Int) = when (index) {
+    0 -> x
+    1 -> y
+    else -> throw IndexOutOfBoundsException("Invalid coordinate $index")
+}
+
+private operator fun <T> MutableCollection<T>.plus(element: T): MutableCollection<T> {
     this.add(element)
     return this;
 }
-private operator fun Point.unaryMinus() = Point(-x, -y)
+
+data class MutablePoint(var x: Int, var y: Int)
+private operator fun MutablePoint.set(index: Int, value: Int) = when(index) {
+    0 -> x = value
+    1 -> y = value
+    else -> throw IndexOutOfBoundsException("Invalid coordinate $index")
+}
 
 fun main(args: Array<String>) {
     val p1 = Point(10, 20)
@@ -31,4 +44,11 @@ fun main(args: Array<String>) {
     val numbers = ArrayList<Int>()
     numbers += 42
     println(numbers)
+
+    val pp = Point(10, 20)
+    println(pp[1])
+
+    val mutablePoint = MutablePoint(10, 20)
+    mutablePoint[1] = 42
+    println(mutablePoint)
 }
